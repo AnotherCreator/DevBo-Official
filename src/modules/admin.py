e8b3adf14a034da06ee4d41400c3833b78fa34e7
AnotherCreator <jreginaldo@cscareer.me>
2020-10-13 19:13:43 -0500

    Add delayed bot notification deletion

--- src/modules/admin.py
+++ src/modules/admin.py
@@ -37,7 +37,7 @@ class Admin(commands.Cog):
                 description=' ',
                 colour=discord.Colour.blurple()
             )
-            await ctx.send(embed=embed)
+            await ctx.send(embed=embed, delete_after=5)
         except discord.ext.commands.errors.MissingRequiredArgument:
             embed = discord.Embed(
                 title='Error: Specify the amount of messages to be removed',
