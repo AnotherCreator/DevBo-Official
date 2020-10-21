51dac719b0c832d20758b109f3a909ab14093ce2
AnotherCreator <jreginaldo@cscareer.me>
2020-10-21 18:45:30 -0500

    Add ';unmute' command

--- src/modules/admin.py
+++ src/modules/admin.py
@@ -64,7 +64,7 @@ class Admin(commands.Cog):
         mute_role = discord.utils.get(ctx.guild.roles, name='Mute')
         await user.add_roles(mute_role)
         embed = discord.Embed(
-            title=f'{user} has been muted',
+            title=f'{user} is muted',
             description=' ',
             colour=discord.Colour.red()
         )
@@ -89,36 +89,15 @@ class Admin(commands.Cog):
 
     @commands.command()
     @commands.has_guild_permissions(manage_roles=True)
-    async def roletest(self, ctx, user: discord.Member):
-        role = discord.utils.get(ctx.guild.roles, name='Role')
-        if ctx.guild.user.has_role(role):
-            await ctx.guild.user.add_roles(role)
-            embed = discord.Embed(
-                title=f'{user} has has been given Role',
-                description=' ',
-                colour=discord.Colour.red()
-            )
-            await ctx.send(embed=embed, delete_after=5)
-        else:
-            await ctx.guild.user.add_roles(role)
-            embed = discord.Embed(
-                title=f'{user} has has been given Role',
-                description=' ',
-                colour=discord.Colour.red()
-            )
-            await ctx.send(embed=embed, delete_after=5)
-
-    # TODO: Add unmute command
-    # This will either be a separate command that removes the 'mute' role
-    # or will use the already made 'mute' command
-
-    # If it uses the 'mute' command ==>
-        # Create some sort of check that determines whether a user has the 'mute' role or not
-        # If the user has the 'mute' role ==> Remove the role
-    # If it uses a separate command ==>
-        # Check if the user has the 'mute' role
-        # If the user has the 'mute' role ==> Remove the role
-        # If the user does not have the 'mute' role ==> Pass an error
+    async def unmute(self, ctx, user: discord.Member):
+        mute_role = discord.utils.get(ctx.guild.roles, name='Mute')
+        await user.remove_roles(mute_role)
+        embed = discord.Embed(
+            title=f'{user} is unmuted',
+            description=' ',
+            colour=discord.Colour.red()
+        )
+        await ctx.send(embed=embed, delete_after=5)
 
 
 # ---       END MAIN        ---#
