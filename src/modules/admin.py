9135968bff3dfce335329b326fd1fcd55b276007
AnotherCreator <jreginaldo@cscareer.me>
2020-10-21 17:47:14 -0500

    Add else/if to ';mute' command to check if user is muted or not

--- src/modules/admin.py
+++ src/modules/admin.py
@@ -66,17 +66,17 @@ class Admin(commands.Cog):
     @commands.has_guild_permissions(manage_roles=True)
     async def mute(self, ctx, user: discord.Member):
         mute_role = discord.utils.get(ctx.guild.roles, name='Mute')
-        if user.has_role(mute_role) != mute_role:
-            await user.add_roles(mute_role)
+        if user.has_role(mute_role):
             embed = discord.Embed(
-                title=f'{user} has been muted',
+                title=f'{user} is now unmute',
                 description=' ',
                 colour=discord.Colour.red()
             )
             await ctx.send(embed=embed, delete_after=5)
         else:
+            await user.add_roles(mute_role)
             embed = discord.Embed(
-                title=f'{user} is already muted',
+                title=f'{user} has been muted',
                 description=' ',
                 colour=discord.Colour.red()
             )
