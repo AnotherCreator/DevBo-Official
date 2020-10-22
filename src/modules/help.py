03eaae0f0fb6621c775e579ae1087b696dcfbb20
AnotherCreator <jreginaldo@cscareer.me>
2020-10-21 19:11:02 -0500

    Update help section to make the commands less confusing

--- src/modules/help.py
+++ src/modules/help.py
@@ -18,7 +18,7 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(
-            title='Use *;more** __**Module Name*__ to get more info',
+            title='Use *;more* __*Module Name*__ to get more info',
             description='• Administration\n'
                         '• Info\n'
                         '• Market\n'
