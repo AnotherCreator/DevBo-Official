4a988db01453dcd78b9f6917549ec2402dbf7bda
AnotherCreator <jreginaldo@cscareer.me>
2020-10-26 17:46:57 -0500

    Add 'updatelogs' command for discord log usage

--- src/modules/botowner.py
+++ src/modules/botowner.py
@@ -10,6 +10,18 @@ class BotOwner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def updatelogs(self, ctx, message):
+        # 'update-notes' channel
+        channel = self.bot.get_channel(768626068629880902)
+        embed = discord.Embed(
+            title=f'{message}',
+            description='',
+            colour=discord.Colour.blurple()
+        )
+
+        await ctx.channel.send(embed=embed)
+
 
 # ---       END MAIN        ---#
 def setup(bot):
