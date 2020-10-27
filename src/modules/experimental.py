cba93567c86ed488526cb77d8c10a02767967bb5
AnotherCreator <jreginaldo@cscareer.me>
2020-10-26 19:06:24 -0500

    Implement adding reactions to embeds

--- src/modules/experimental.py
+++ src/modules/experimental.py
@@ -18,8 +18,9 @@ class Experimental(commands.Cog):
 
     @commands.command()
     async def reactiontext(self, ctx, message):
-        emoji = ['◀', '▶']
-        await ctx.message.add_reaction(emoji)
+        emoji1 = '◀'
+        emoji2 = '▶'
+        await ctx.message.add_reaction(emoji1, emoji2)
 
     @commands.command()
     async def reactionembed(self, ctx):
