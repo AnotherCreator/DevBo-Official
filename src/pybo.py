bf3cc0f82f3552977dc723ea185318a5172b2b59
AnotherCreator <jreginaldo@cscareer.me>
2020-11-02 17:16:45 -0600

    Remove '_levels.py'

--- src/pybo.py
+++ src/pybo.py
@@ -6,29 +6,33 @@ from discord.ext import commands, tasks
 from dotenvy import load_env, read_file
 from itertools import cycle
 
-# ---       GLOBAL VARIABLES          ---#
+# ---       ENV VARIABLES       ---#
+
 load_env(read_file('.env'))
+# Bot / Bot Owner related
 SECRET_KEY = os.environ.get('SECRET_KEY')
 OWNER_ID = os.environ.get('OWNER_ID')
-PG_PW = os.environ.get('PG_PW')
 
+# PostgreSQL related
+PG_PW = os.environ.get('PG_PW')
 DB_URL = os.environ.get('DB_URL')
 conn = psycopg2.connect(DB_URL, sslmode='require')
+
+# ---     BOT INITIALIZATION    --- #
 
 bot = commands.Bot(command_prefix=';')
 bot.remove_command('help')
 
+# ---     GLOBAL VARIABLES      --- #
+
 status = cycle(['For more info | ;help', 'Under development! | ;help'])
 
 
-# ---       FUNCTIONS           --- #
+# ---         FUNCTIONS         --- #
+
 def bot_owner_check(ctx):
     if ctx.author.id == int(OWNER_ID):
         return ctx.author.id
-
-
-# async def create_db_pool():
-#    conn = psycopg2.connect(DB_URL, sslmode='require')
 
 
 # ---       BACKGROUND STUFF    --- #
@@ -76,6 +80,11 @@ async def updateissues(ctx, *, message):
 
 # ---       LOAD MODULES        --- #
 
+for filename in os.listdir('./modules'):
+    if filename.endswith('.py') and not filename.startswith('_'):
+        bot.load_extension(f'modules.{filename[:-3]}')
+
+
 @bot.command()
 @commands.check(bot_owner_check)
 async def load(ctx, extension):
@@ -90,11 +99,6 @@ async def unload(ctx, extension):
     print(f'{extension} has been unloaded')
 
 
-for filename in os.listdir('./modules'):
-    if filename.endswith('.py') and not filename.startswith('_'):
-        bot.load_extension(f'modules.{filename[:-3]}')
-
-
 @bot.command()
 @commands.check(bot_owner_check)
 async def reload(ctx, extension):
@@ -103,5 +107,4 @@ async def reload(ctx, extension):
 
 
 # ---       END MAIN            ---#
-# bot.loop.run_until_complete(create_db_pool())
 bot.run(SECRET_KEY)
