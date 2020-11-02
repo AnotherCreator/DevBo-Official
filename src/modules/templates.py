f5f7060749164d53c5acd42766b2d5d2035bbcd8
AnotherCreator <jreginaldo@cscareer.me>
2020-11-02 11:16:48 -0600

    Change 'template.py' to test grabbing user avatar and name

--- src/modules/templates.py
+++ src/modules/templates.py
@@ -26,8 +26,8 @@ class Templates(commands.Cog):
             url='https://cdn.discordapp.com/attachments/445104854327623692/604275896202821632'
                 '/Self-potrait_Shading.png')
         embed.set_author(
-            name=f'{ctx.user.name}',
-            icon_url=f'{self.bot.Discord.avatar_url}'
+            name=f'{self.bot.discord.User.name}',
+            icon_url=f'{self.bot.discord.User.avatar_url}'
         )
         embed.add_field(name='Field Name', value='Field Value', inline=False)
         embed.add_field(name='Field Name', value='Field Value', inline=True)
