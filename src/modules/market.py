7c32573ed0174bc43164b6eb3630bde0c6183ff5
AnotherCreator <jreginaldo@protonmail.com>
2021-03-08 17:39:44 -0600

    Add coin symbol / abbreviations

--- src/modules/market.py
+++ src/modules/market.py
@@ -26,6 +26,7 @@ soup2 = BeautifulSoup(page2, 'html.parser')
 # dicts
 coin_icons = {}
 coin_names = {}
+coin_abbreviation = {}
 coin_prices = {}
 coin_percent_change = {}
 
@@ -50,6 +51,15 @@ def names():
     for name in soup.find_all('div', class_='sc-1kxikfi-0 fjclfm cmc-table__column-name'):
         name = name.text
         coin_names[counter] = name
+        counter += 1
+
+
+def abbreviations():
+    counter = 1
+    for abb in soup.find_all('td', class_='cmc-table__cell cmc-table__cell--sortable '
+                                          'cmc-table__cell--left cmc-table__cell--sort-by__symbol'):
+        abb = abb.text
+        coin_abbreviation[counter] = abb
         counter += 1
 
 
@@ -90,6 +100,7 @@ class Market(commands.Cog):
     @commands.check(bot_channel_check)
     async def crypto(self, ctx, coin_number):
         # Function calls
+        abbreviations()
         names()
         icons()
         prices()
@@ -106,13 +117,13 @@ class Market(commands.Cog):
         embed.set_footer(text=site)
         if 0 < int(coin_number) <= 10:
             embed.set_author(
-                name=f'{coin_number}. {str(coin_names.get(coin_number))}',
+                name=f'{coin_number}. {str(coin_names.get(coin_number))} / {str(coin_abbreviation.get(coin_number))}',
                 icon_url=coin_icons.get(int(coin_number))
             )
-            embed.add_field(name='% 24h', value=str(coin_percent_change.get(coin_number)), inline=False)
+            embed.add_field(name='24h % Change', value=str(coin_percent_change.get(coin_number)), inline=False)
         elif 11 <= int(coin_number) <= 50:
             embed.set_author(
-                name=f'{coin_number}. {str(coin_names.get(coin_number))}'
+                name=f'{coin_number}. {str(coin_names.get(coin_number))} / {str(coin_abbreviation.get(coin_number))}'
             )
             embed.add_field(name='24h % Change', value=str(coin_percent_change.get(coin_number)), inline=False)
         else:
@@ -130,6 +141,7 @@ class Market(commands.Cog):
     @commands.check(bot_channel_check)
     async def cryptolist(self, ctx, page):
         # Function Calls
+        abbreviations()
         names()
         prices()
         # Variables
@@ -148,35 +160,35 @@ class Market(commands.Cog):
         if page == 1:
             counter = 1
             while counter < 11:
-                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))}',
+                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))} / {str(coin_abbreviation.get(counter))}',
                                 value=str(coin_prices.get(counter)),
                                 inline=False)
                 counter += 1
         elif page == 2:
             counter = 11
             while counter < 21:
-                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))}',
+                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))} / {str(coin_abbreviation.get(counter))}',
                                 value=str(coin_prices.get(counter)),
                                 inline=False)
                 counter += 1
         elif page == 3:
             counter = 21
             while counter < 31:
-                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))}',
+                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))} / {str(coin_abbreviation.get(counter))}',
                                 value=str(coin_prices.get(counter)),
                                 inline=False)
                 counter += 1
         elif page == 4:
             counter = 31
             while counter < 41:
-                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))}',
+                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))} / {str(coin_abbreviation.get(counter))}',
                                 value=str(coin_prices.get(counter)),
                                 inline=False)
                 counter += 1
         elif page == 5:
             counter = 41
             while counter < 51:
-                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))}',
+                embed.add_field(name=f'{counter}. {str(coin_names.get(counter))} / {str(coin_abbreviation.get(counter))}',
                                 value=str(coin_prices.get(counter)),
                                 inline=False)
                 counter += 1
