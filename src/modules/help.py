99ba956679387509602b2ee57a051577a9268942
AnotherCreator <jreginaldo@protonmail.com>
2021-05-07 22:12:07 -0500

    Refactor src

---
+++ src/modules/help.py
@@ -0,0 +1,113 @@
+# ---       IMPORTS         --- #
+
+
+import discord
+from discord.ext import commands
+
+# ---   GLOBAL VARIABLES    --- #
+
+bot_avatar_link = 'https://cdn.discordapp.com/avatars/733004304855597056/d55234172599dca4b11e6345078a32b0.png?size=128'
+
+# ---     CUSTOM CHECKS     --- #
+
+
+def bot_channel_check(ctx):
+    botspam_channels = ['bot-spam', 'bot-commands']
+    if str(ctx.message.channel) in botspam_channels or ctx.author.id == 291005201840734218:
+        return True
+
+# ---       MAIN LINE       --- #
+
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.check(bot_channel_check)
+    async def help(self, ctx):
+        embed = discord.Embed(
+            title='Use *;more* __*Module Name*__ to get more info',
+            description='• Administration\n'
+                        '• Info\n'
+                        '• Market\n'
+                        '• Experimental',
+            colour=discord.Colour.blurple()
+        )
+        embed.set_author(name='Commands List',
+                         icon_url=bot_avatar_link)
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.check(bot_channel_check)
+    async def more(self, ctx, module):
+        if module == 'Administration' or module == 'administration' or module == 'Admin' or module == 'admin':
+            embed = discord.Embed(
+                title='Administration',
+                description=' ',
+                colour=discord.Colour.blurple()
+            )
+            embed.set_author(name='Commands List', icon_url=bot_avatar_link)
+            embed.add_field(name=';prune __Amount__',
+                            value='Removes the amount of messages specified',
+                            inline=False)
+            embed.add_field(name=';mute __@user / user ID__',
+                            value='Mutes the specified user',
+                            inline=False)
+            embed.add_field(name=';unmute __@user / user ID__',
+                            value='Unmutes the specified user',
+                            inline=False)
+            embed.set_footer(text='User IDs can be found by turning on "Developer Mode"')
+
+            await ctx.send(embed=embed)
+        elif module == 'Info' or module == 'info':
+            embed = discord.Embed(
+                title='Info',
+                description=' ',
+                colour=discord.Colour.blurple()
+            )
+            embed.set_author(name='Commands List', icon_url=bot_avatar_link)
+            embed.add_field(name=';ping', value='pong!',
+                            inline=False)
+            await ctx.send(embed=embed)
+        elif module == 'Market' or module == 'market':
+            embed = discord.Embed(
+                title='Market',
+                description=' ',
+                colour=discord.Colour.blurple()
+            )
+            embed.set_author(name='Commands List', icon_url=bot_avatar_link)
+            embed.add_field(name=';crypto __1-50__',
+                            value='Displays Name / Current Price / Percent Change',
+                            inline=False)
+            embed.add_field(name=';cryptolist __1-5__',
+                            value='Displays the top 50 coins',
+                            inline=False)
+
+            await ctx.send(embed=embed)
+        elif module == 'Experimental' or module == 'experimental' or module == 'Experiment' or module == 'experiment':
+            embed = discord.Embed(
+                title='Experimental',
+                description=' ',
+                colour=discord.Colour.blurple()
+            )
+            embed.set_author(name='Commands List', icon_url=bot_avatar_link)
+            embed.add_field(name=';pic',
+                            value='Displays a randomly AI generated character',
+                            inline=False)
+            # embed.add_field(name=';stool',
+            #                 value='Your portable high-ground',
+            #                 inline=False)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                title='Invalid Command',
+                description=' ',
+                colour=discord.Colour.red()
+            )
+            await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Help(bot))
---
+++ src/modules/templates.py
@@ -0,0 +1,45 @@
+# ---       IMPORTS          ---#
+
+import discord
+from discord.ext import commands
+
+# ---       MAIN LINE       ---#
+
+
+class Templates(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def template(self, ctx):
+        embed = discord.Embed(
+            title='Title',
+            description='This is a description',
+            colour=discord.Colour.blurple()
+        )
+        embed.set_footer(text='This is a footer.')
+        embed.set_image(
+            url=ctx.author.avatar_url
+        )
+        embed.set_thumbnail(
+            url='https://cdn.discordapp.com/attachments/445104854327623692/604275896202821632'
+                '/Self-potrait_Shading.png')
+        embed.set_author(
+            name=ctx.author,
+            icon_url='https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.png'
+        )
+        embed.add_field(name='Field Name', value='Field Value', inline=False)
+        embed.add_field(name='Field Name', value='Field Value', inline=True)
+        embed.add_field(name='Field Name', value='Field Value', inline=True)
+
+        await ctx.send(embed=embed)
+
+    # TODO: Add a server 'template' that allows a user to type a command which makes the bot send multiple ...
+    # TODO: ... messages that introduces all the majority of its commands (excluding administration commands)
+    # TODO: Administration commands can go in a separate 'mod only' chat
+
+
+# ---       END MAIN        ---#
+def setup(bot):
+    bot.add_cog(Templates(bot))
