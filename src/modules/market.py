7824469c45486c4d08fc85ce862019c4f2633063
AnotherCreator <jreginaldo@protonmail.com>
2021-05-12 10:16:20 -0500

    Implement left page turning on 'coin' command

--- src/modules/market.py
+++ src/modules/market.py
@@ -111,7 +111,7 @@ def update_coins():
 
 
 def query_coins(rank):
-    cur.execute('SELECT * FROM coin_info ORDER BY coin_rank asc')
+    cur.execute('SELECT * FROM coin_info ORDER BY coin_rank asc')  # 1,2,3...,100
     rows = cur.fetchall()
 
     if rank < 11:
@@ -139,6 +139,25 @@ def query_coins(rank):
                 name=f'{x[5]}. {x[1]} / {x[2]}',
                 value=f'${x[3]}',
                 inline=False)
+    return embed
+
+
+def get_left_coin(current_page):
+    cur.execute("SELECT * FROM coin_info WHERE coin_rank = %s", (current_page,))  # 1,2,3...,100
+    rows = cur.fetchall()
+
+    # ID: x[0] || Name: x[1] || Symbol: x[2] || Price: x[3] || Logo: x[4] || Rank: x[5]
+    for x in rows:
+        embed = discord.Embed(
+            title=f'${str(x[3])}',
+            description=' ',
+            colour=discord.Colour.blurple()
+        )
+        embed.set_author(
+            name=f'{x[5]}. {x[1]} / {x[2]}',
+            icon_url=x[4]
+        )
+        embed.set_footer(text="")
     return embed
 
 
@@ -194,6 +213,7 @@ class Market(commands.Cog):
         cur.execute('SELECT * FROM coin_info')
         rows = cur.fetchall()
 
+        await ctx.message.delete()  # Deletes command call
         for x in rows:
             #  ID: x[0] || Name: x[1] || Symbol: x[2] || Price: x[3] || Logo: x[4] || Rank: x[5]
             if x[1] == name or x[5] == int(name):
@@ -213,23 +233,42 @@ class Market(commands.Cog):
                     await message.add_reaction(emoji)
 
                 check = reaction_check(message=message, author=ctx.author, emoji=(emoji_list[0], emoji_list[1]))
+                current_page = x[5]
+
                 while True:
                     try:
                         reaction, user = await self.bot.wait_for('reaction_add', timeout=10.0, check=check)
-                        if reaction.emoji == emoji_list[0]:
+                        if reaction.emoji == emoji_list[0]:  # Left page
+                            await message.delete()  # Deletes embed before sending a new one
+                            current_page = current_page - 1
+                            embed = get_left_coin(current_page)
+                            message = await ctx.send(embed=embed)
+
+                            for emoji in emoji_list:
+                                await message.add_reaction(emoji)
+
+                            check = reaction_check(message=message, author=ctx.author,
+                                                   emoji=(emoji_list[0], emoji_list[1]))
+
+                        elif reaction.emoji == emoji_list[1]:  # Right page
+                            await message.delete()
                             embed = discord.Embed(
-                                title=f'Left',
+                                title=f'Hi',
                                 description=' ',
                                 colour=discord.Colour.blurple()
                             )
-                            await message.edit(content=None, embed=embed)
-                        elif reaction.emoji == emoji_list[1]:
-                            embed = discord.Embed(
-                                title=f'right',
-                                description=' ',
-                                colour=discord.Colour.blurple()
+                            embed.set_author(
+                                name=f'Joe',
+                                icon_url=x[4]
                             )
-                            await message.edit(content=None, embed=embed)
+                            embed.set_footer(text="")
+                            message = await ctx.send(embed=embed)
+
+                            for emoji in emoji_list:
+                                await message.add_reaction(emoji)
+
+                            check = reaction_check(message=message, author=ctx.author,
+                                                   emoji=(emoji_list[0], emoji_list[1]))
                     except TimeoutError:
                         print('Timeout')
 
