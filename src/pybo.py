02c9bd0710e858fc52f718813e960d027652af02
AnotherCreator <jreginaldo@protonmail.com>
2021-05-16 19:15:58 -0500

    self.bot.cur changed to .pg_con

--- src/pybo.py
+++ src/pybo.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     async def create_db_pool():
         # 'self.bot.pg_con' to connect to db in /module files
-        bot.pg_con = await asyncpg.create_pool(database='DB_NAME', user='DB_USER', password=DB_PW)
+        bot.pg_con = await asyncpg.create_pool(database=DB_NAME, user=DB_USER, password=DB_PW)
 
     # ---       MODULE IMPORTS             --- #
     # Module imports cant be at the top because 'pybo.py' has to first load all the modules
@@ -113,4 +113,5 @@ if __name__ == '__main__':
 
     # ---       END MAIN            ---#
     bot.loop.create_task(refresh_coins())
+    bot.loop.run_until_complete(create_db_pool())
     bot.run(SECRET_KEY)
