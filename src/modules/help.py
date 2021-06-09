17a6ffc42fa829de47a0b739cf4339aebc82d8aa
AnotherCreator <jreginaldo@protonmail.com>
2021-06-09 08:14:30 -0500

    Implement '/help' command

--- src/modules/help.py
+++ src/modules/help.py
@@ -1,6 +1,9 @@
 # ---       IMPORTS         --- #
 import discord
+
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 from pybo import BOT_AVATAR
 
 
@@ -16,22 +19,40 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.check(bot_channel_check)
-    async def help(self, ctx, module=None):
-        if module is None:
-            embed = discord.Embed(
-                title='Use *;help* __*Module Name*__ to get more info',
-                description='• Administration\n'
-                            '• Info\n'
-                            '• Market\n'
-                            '• Experimental',
-                colour=discord.Colour.blurple()
+    # 746153452606062652 = Dev server
+    # 823595529250275378 = Water sapiens
+    guild_ids = [746153452606062652, 823595529250275378]
+
+    # ---       SLASH COMMANDS       --- #
+    @cog_ext.cog_slash(
+        name='help',
+        description='List of bot commands!',
+        guild_ids=guild_ids,
+        options=[
+            create_option(
+                name='module',
+                description='Choose category!',
+                required=True,
+                option_type=3,
+                choices=[
+                    create_choice(
+                        name='Administration',
+                        value='admin'
+                    ),
+                    create_choice(
+                        name='Info',
+                        value='info'
+                    ),
+                    create_choice(
+                        name='Market',
+                        value='market'
+                    )
+                ]
             )
-            embed.set_author(name='Commands List',
-                             icon_url=BOT_AVATAR)
-            await ctx.send(embed=embed)
-        elif module == 'Administration' or module == 'administration' or module == 'Admin' or module == 'admin':
+        ]
+    )
+    async def _help(self, ctx: SlashContext, module: str):
+        if module == 'admin':
             embed = discord.Embed(
                 title='Administration',
                 description=' ',
@@ -48,9 +69,7 @@ class Help(commands.Cog):
                             value='Unmutes the specified user',
                             inline=False)
             embed.set_footer(text='User IDs can be found by turning on "Developer Mode"')
-
-            await ctx.send(embed=embed)
-        elif module == 'Info' or module == 'info':
+        elif module == 'info':
             embed = discord.Embed(
                 title='Info',
                 description=' ',
@@ -59,8 +78,7 @@ class Help(commands.Cog):
             embed.set_author(name='Commands List', icon_url=BOT_AVATAR)
             embed.add_field(name=';ping', value='pong!',
                             inline=False)
-            await ctx.send(embed=embed)
-        elif module == 'Market' or module == 'market':
+        elif module == 'market':
             embed = discord.Embed(
                 title='Market',
                 description=' ',
@@ -73,15 +91,74 @@ class Help(commands.Cog):
             embed.add_field(name=';top __1-100__',
                             value='Displays the top # coins',
                             inline=False)
+        await ctx.send(embeds=[embed])
 
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title='Invalid Command',
-                description=' ',
-                colour=discord.Colour.red()
-            )
-            await ctx.send(embed=embed)
+    # ---       PREFIX COMMANDS       --- #
+    # @commands.command()
+    # @commands.check(bot_channel_check)
+    # async def help(self, ctx, module=None):
+    #     if module is None:
+    #         embed = discord.Embed(
+    #             title='Use *;help* __*Module Name*__ to get more info',
+    #             description='• Administration\n'
+    #                         '• Info\n'
+    #                         '• Market\n',
+    #             colour=discord.Colour.blurple()
+    #         )
+    #         embed.set_author(name='Commands List',
+    #                          icon_url=BOT_AVATAR)
+    #         await ctx.send(embed=embed)
+    #     elif module == 'Administration' or module == 'administration' or module == 'Admin' or module == 'admin':
+    #         embed = discord.Embed(
+    #             title='Administration',
+    #             description=' ',
+    #             colour=discord.Colour.blurple()
+    #         )
+    #         embed.set_author(name='Commands List', icon_url=BOT_AVATAR)
+    #         embed.add_field(name=';prune __Amount__',
+    #                         value='Removes the amount of messages specified',
+    #                         inline=False)
+    #         embed.add_field(name=';mute __@user / user ID__',
+    #                         value='Mutes the specified user',
+    #                         inline=False)
+    #         embed.add_field(name=';unmute __@user / user ID__',
+    #                         value='Unmutes the specified user',
+    #                         inline=False)
+    #         embed.set_footer(text='User IDs can be found by turning on "Developer Mode"')
+    #
+    #         await ctx.send(embed=embed)
+    #     elif module == 'Info' or module == 'info':
+    #         embed = discord.Embed(
+    #             title='Info',
+    #             description=' ',
+    #             colour=discord.Colour.blurple()
+    #         )
+    #         embed.set_author(name='Commands List', icon_url=BOT_AVATAR)
+    #         embed.add_field(name=';ping', value='pong!',
+    #                         inline=False)
+    #         await ctx.send(embed=embed)
+    #     elif module == 'Market' or module == 'market':
+    #         embed = discord.Embed(
+    #             title='Market',
+    #             description=' ',
+    #             colour=discord.Colour.blurple()
+    #         )
+    #         embed.set_author(name='Commands List', icon_url=BOT_AVATAR)
+    #         embed.add_field(name=';coin __1-100__ or ;coin __name__',
+    #                         value='Displays Name / Current Price',
+    #                         inline=False)
+    #         embed.add_field(name=';top __1-100__',
+    #                         value='Displays the top # coins',
+    #                         inline=False)
+    #
+    #         await ctx.send(embed=embed)
+    #     else:
+    #         embed = discord.Embed(
+    #             title='Invalid Command',
+    #             description=' ',
+    #             colour=discord.Colour.red()
+    #         )
+    #         await ctx.send(embed=embed)
 
 
 # ---       END MAIN            ---#
